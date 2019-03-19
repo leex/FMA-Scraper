40745d273866f0a1d9b3f36dea54977391c69db5
Alex Jacobsen <alex@siffin.co.nz>
2019-03-20 06:26:10 +1300

    Scripts: Updated and modified to work with search page.
    Also added offset command line argument.

--- Scripts/FMA Downloader.py
+++ Scripts/FMA Downloader.py
@@ -1,20 +1,49 @@
-import os, sys
+#!/usr/bin/env python3
+import os, sys, requests, re
 
+# The search string, you should change this to what you want to search for.
+search_string = 'KEXP'
+
+# Get the path where this script is currently executing
 def getScriptPath():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
-URLs = []
-files = os.listdir(getScriptPath())
+# Remove the quotation marks at the end and beginning of the filename
+def removeQuotationMarks(filename):
+	return filename[1:-1]
 
-for file in files:
-	if os.path.splitext(file)[1] == '.fma':
-		directory = os.path.splitext(file)[0]
-		if not os.path.exists(getScriptPath() + '\\' + directory):
-			os.makedirs(directory)
-		with open(file) as f:
-			URLs = f.readlines()
-        for URL in URLs:
-            os.system('python -m wget -o %s %s' %(directory, URL))
-            with open(file, 'w') as f2:
-                f2.writelines(URLs[1:])
-                URLs.pop(0)
+# Create the output directory if it does not already exist
+def createOutputDirectory(directory):
+	if not os.path.exists(getScriptPath() + '\\' + directory):
+		os.makedirs(directory)
+
+URLs = []
+files_in_working_directory = os.listdir(getScriptPath())
+
+for file in files_in_working_directory:
+	file_is_dot_fma = os.path.splitext(file)[1] == '.fma'
+
+	if file_is_dot_fma:
+		directory = 'songs'
+
+		createOutputDirectory(directory)
+
+		with open(file) as fma_file:
+			URLs = fma_file.readlines()
+
+		for URL in URLs:
+			response = requests.get(URL.strip(), stream=True, headers={'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36'})
+
+			# If request is successful
+			if response.status_code == 200:
+
+				# Get the filename from the headers
+				song_headers = response.headers['content-disposition']
+				song_filename = re.findall("filename=(.+)", song_headers)
+				song_filename = song_filename[0]
+
+				song_filename = removeQuotationMarks(song_filename)
+
+				# Write the song file
+				with open('songs/' + song_filename, 'wb') as f2:
+					f2.write(response.content);
--- Scripts/FMA Scraper.py
+++ Scripts/FMA Scraper.py
@@ -1,22 +1,63 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import sys, requests, re, codecs
+import argparse
 
-import requests, re, codecs
+parser = argparse.ArgumentParser(description='Scrape download links from the FMA website.')
+parser.add_argument('integers', metavar='N', default=10, type=int, nargs='*',
+                    help='an integer stating how many links to ignore from the start of the scrape.')
 
-regex = re.compile('((http:\/\/freemusicarchive.org\/music\/download\/)\w+)')
-genres = ['Dance', 'Downtempo', 'Drum_amp_Bass', 'Dubstep', 'Skweee', 'Glitch', 'House', 'Chill-out', 'IDM', 'Jungle', 'Minimal_Electronic', 'Techno', 'Bigbeat','Trip-Hop']
-genreCount = 0
+### This scraper uses the search page to find tracks then creates a file with a list of download links
+args = vars(parser.parse_args())
 
-while genreCount < 13:
-    genre = genres[genreCount]
-    URL = 'http://freemusicarchive.org/genre/' + genre + '/?sort=track_date_published&d=120&page=1&per_page=200'
-    r = requests.get(URL)
-    content = r.text
-    file = codecs.open(genre + '.fma', 'w+', 'utf-8')
-    filteredContent = re.findall(regex, content)
-    for item in filteredContent:
-        file.write(item[0])
-        file.write('\n')
-    file.close()
-    print genre + ' list populated!'
-    genreCount += 1
+print("\nstarting scraping\n")
+
+# The search string, you should change this to what you want to search for
+search_string = 'KEXP'
+
+# How many records to ignore at the beginning of the scrap (if you've already scraped some)
+count_offset = args['integers']
+
+print("offset set to " + str(count_offset) + ".")
+
+# The name of the file to create
+filename = 'scraped_download_links'
+
+# The number of items to return in the search and scrape, I have tested up to 1000
+number_of_items_to_search = '100'
+
+# The regex to find the download link, you probably don't need to change this
+regex_find_download_link = re.compile('((https:\/\/freemusicarchive.org\/music\/download\/)\w+)')
+
+# Here we open the file we will write the links to
+file = codecs.open(filename + '.fma', 'w+', 'utf-8')
+
+URL = 'http://freemusicarchive.org/search/?quicksearch=' + search_string + '&per_page=' + number_of_items_to_search
+r = requests.get(URL)
+content = r.text
+filteredContent = re.findall(regex_find_download_link, content)
+
+scraped_links_count = str(len(filteredContent))
+
+print("found " + scraped_links_count + " songs.")
+
+# Write one line to the file for each link
+count = 0
+for item in filteredContent:
+    # Ignore lines we don't want at the beginning
+    count = count + 1
+    if count <= count_offset:
+        continue
+
+    # Write to the file
+    file.write(item[0])
+    file.write('\n')
+
+file.close()
+
+print("file '" + filename + ".fma' created.")
+print("processed " + str(int(scraped_links_count) - count_offset) + " links.")
+
+print("\nending scraping\n")
+
+# @todo use the search_string to create the file but first we need to escape bad characters
+# @todo create offset to being downloading after first x songs
